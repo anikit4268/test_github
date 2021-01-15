e9fe544ea52d22489d9ee8eb8a1a70528bd57578
Arthur N <arthur.nikitin.2015@gmail.com>
2021-01-14 20:59:36 -0600

    new version

--- Program.cs
+++ Program.cs
@@ -2,11 +2,33 @@
 
 namespace test_github
 {
+    class Person
+    {
+        public string name;
+        static public string personality;
+        public Car car;
+
+    }
+
+    class Car
+    {
+        public string  color;
+        static public string make;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 11111!");
+            var person1 = new Person();
+            var person2 = new Person();
+            var car1 = new Car();
+            var car2 = new Car();
+
+            Console.WriteLine("Enter name: ");
+            string input = Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
+
